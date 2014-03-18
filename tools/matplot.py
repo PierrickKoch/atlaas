b3093ac4ef257ca53fb945172809a180f7bf3158
Pierrick Koch <pierrick.koch@gmail.com>
2014-03-18 13:47:27 +0100

    [tools] add ipython example
    see http://nbviewer.ipython.org/gist/pierriko/9596093 for result demo

--- tools/matplot.py
+++ tools/matplot.py
@@ -1,46 +1,73 @@
+# -*- coding: utf-8 -*-
+# <nbformat>2</nbformat>
+
+# <codecell>
+
+
 filename = 'out.tif'
 
 import gdal
-g = gdal.Open( filename )
-names = {v:k+1 for k,v in enumerate(["N_POINTS", "Z_MIN", "Z_MAX", "Z_MEAN", "VARIANCE", "TIME"])}
-'''
-b1 = g.GetRasterBand(names['N_POINTS'])
-r1 = b1.ReadAsArray()
-b5 = g.GetRasterBand(names['VARIANCE'])
-r5 = b5.ReadAsArray()
-
-f1 = r1.flatten()
-f5 = r5.flatten()
-t = 0
-c = 0
-# todo numpy must have smart way of doing this
-for n,v in zip(f1, f5):
-    if n > 1:
-        t += v
-        c += 1
-
-m = t / c
-m * 3.
-'''
-# import numpy as np
-# import matplotlib.mlab as mlab
-# or run `ipython --pylab`
+# run `ipython notebook --pylab inline`
 import matplotlib.pyplot as plt
-'''
-f4 = r4.flatten()
-plt.hist( f4, 256 )
-plt.show()
-'''
-g = gdal.Open( filename )
-b4 = g.GetRasterBand(names['Z_MEAN'])
-r4 = b4.ReadAsArray()
+names = {v:k for k,v in enumerate(["N_POINTS", "Z_MIN", "Z_MAX", "Z_MEAN", "VARIANCE", "TIME"])}
+geotiff = gdal.Open( filename )
+bands   = geotiff.ReadAsArray()
 
-imgplot = plt.imshow( r4 )
-imgplot.set_clim(-1.0, 3.0)
+# <codecell>
+
+band = bands[names['N_POINTS']]
+plt.subplots(figsize=(10, 10))
+plt.semilogy()
+plt.hist( band.flatten(), 256 )
+plt.show()
+
+# <codecell>
+
+band = bands[names['N_POINTS']]
+plt.subplots(figsize=(14, 14))
+imgplot = plt.imshow( band )
+imgplot.set_clim(0, 500)
 plt.colorbar()
 plt.show()
-imgplot.figure.savefig('%s.pdf'%filename)
-imgplot.write_png('%s.png'%filename)
-# http://matplotlib.org/users/image_tutorial.html
-# https://github.com/assimp/assimp/blob/master/port/PyAssimp/README.md
-# http://grass.osgeo.org/grass70/manuals/r.viewshed.html
+
+# <codecell>
+
+band = bands[names['VARIANCE']]
+plt.subplots(figsize=(10, 10))
+plt.semilogy()
+plt.hist( band.flatten(), 256 )
+plt.show()
+
+# <codecell>
+
+band = bands[names['VARIANCE']]
+plt.subplots(figsize=(14, 14))
+imgplot = plt.imshow( band )
+imgplot.set_clim(0, 0.2)
+imgplot.set_cmap('spectral')
+plt.colorbar()
+plt.show()
+
+# <codecell>
+
+band = bands[names['Z_MEAN']]
+plt.subplots(figsize=(10, 10))
+plt.semilogy()
+plt.hist( band.flatten(), 256 )
+plt.show()
+
+# <codecell>
+
+band = bands[names['Z_MEAN']]
+plt.subplots(figsize=(14, 14))
+imgplot = plt.imshow( band )
+imgplot.set_clim(-0.5, 4.0)
+plt.colorbar()
+plt.show()
+
+# <markdowncell>
+
+# - http://nbviewer.ipython.org/9596093
+# - http://matplotlib.org/users/image_tutorial.html
+# - https://github.com/assimp/assimp/blob/master/port/PyAssimp/README.md
+# - http://grass.osgeo.org/grass70/manuals/r.viewshed.html
