b91054619020bec558afa3a92afc727fbcc0f26a
Pierrick Koch <pierrick.koch@gmail.com>
2014-07-30 17:17:28 +0200

    cython

--- python/atlaas.pyx
+++ python/atlaas.pyx
@@ -1,4 +1,34 @@
-cimport atlaas
+# import dereference and increment operators
+from cython.operator cimport dereference as deref, preincrement as inc
+
+cdef extern from "<array>" namespace "std":
+    cdef cppclass array[T, N]:
+        cppclass iterator:
+            T operator*()
+            iterator operator++()
+            bint operator==(iterator)
+            bint operator!=(iterator)
+        array()
+        T& operator[](int)
+        T& at(int)
+        iterator begin()
+        iterator end()
+
+from libcpp cimport bool
+from libcpp.vector cimport vector
+
+cdef extern from "../include/atlaas/atlaas.hpp" namespace "atlaas":
+    #ctypedef vector[array[double, 4]] points
+    #ctypedef array[double, 16] matrix
+    cdef cppclass atlaas:
+        atlaas()
+        void init(double size_x, double size_y, double scale,
+              double custom_x, double custom_y, double custom_z,
+              int utm_zone, bool utm_north) except +
+        void init(double size_x, double size_y, double scale,
+              double custom_x, double custom_y, double custom_z,
+              int utm_zone) except +
+        #void merge(points& cloud, const matrix& transformation)
 
 cdef class PyAtlaas:
     cdef atlaas *thisptr # hold a C++ instance which we're wrapping
@@ -14,3 +44,5 @@ cdef class PyAtlaas:
               utm_zone):
         self.thisptr.init(size_x, size_y, scale, custom_x, custom_y, custom_z,
               utm_zone)
+
+
