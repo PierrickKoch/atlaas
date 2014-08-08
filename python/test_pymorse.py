a03d6bc2d9f4a792f869523fd4c9e057971a87f7
Pierrick Koch <pierrick.koch@gmail.com>
2014-08-08 13:55:01 +0200

    cython pose

--- python/test_pymorse.py
+++ python/test_pymorse.py
@@ -5,7 +5,7 @@ import logging
 import atlaas
 import pymorse
 import numpy as np
-import transformations as tf
+from transformations import euler_matrix
 
 logger = logging.getLogger("pymorse")
 console = logging.StreamHandler()
@@ -14,16 +14,10 @@ console.setFormatter(formatter)
 logger.setLevel(logging.INFO)
 logger.addHandler(console)
 
-def msg_to_cloud_struct(msg):
-    data = base64.b64decode( msg['points'] )
-    return [ list( struct.unpack('fff', data[i:i+12]) ) + [0.0]
-             for i in range(0, len(data) - 12, 12) ]
-
 def msg_to_cloud_numpy(msg):
     data = base64.b64decode( msg['points'] )
     arr  = np.frombuffer(data, dtype=np.float32)
     return arr.reshape(arr.size/3, 3)
-    # return [list(p)+[0.0] for p in arr]
 
 def get_mat(cfg):
     otr = cfg.result()['configurations']['object_to_robot']
@@ -37,7 +31,7 @@ def get_mat(cfg):
 def get_pose(pose):
     rotation = [pose[k] for k in ('pitch','roll','yaw')]
     translation = [pose[k] for k in ('x','y','z')]
-    M = tf.euler_matrix(*rotation)
+    M = euler_matrix(*rotation)
     M[:3, 3] = translation[:3]
     return M
 
@@ -48,8 +42,8 @@ def main():
         cfg = morse.robot.camera.get_configurations()
         cam_mat = get_mat( cfg )
         while morse.is_up():
-            msg = morse.robot.camera.get()
             pose = morse.robot.pose.get()
+            msg = morse.robot.camera.get()
             cloud = msg_to_cloud_numpy( msg )
             rob_mat = get_pose(pose)
             tr = rob_mat.dot(cam_mat)
