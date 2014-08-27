dd564f78093bda4d7d1fc49ccd35e133cfc02e71
Pierrick Koch <pierrick.koch@gmail.com>
2014-08-27 10:14:35 +0200

    [python] fromstring -> frombuffer dont copy data

--- python/test_ros.py
+++ python/test_ros.py
@@ -1,3 +1,4 @@
+import time
 import rospy
 from sensor_msgs.msg import PointCloud2
 from tf import TransformListener, transformations
@@ -6,6 +7,7 @@ import atlaas
 
 test = atlaas.Atlaas()
 test.init(120.0, 120.0, 0.1, 0, 0, 0, 31, True)
+profile = []
 
 def transformation(frame, header):
     tfl.waitForTransform(frame, header.frame_id, header.stamp, rospy.Duration(3.0) )
@@ -14,12 +16,22 @@ def transformation(frame, header):
     M[:3, 3] = position[:3]
     return M
 
+def cloud(data):
+    buff = np.frombuffer(data, dtype=np.float32)
+    return buff.reshape(buff.size/3, 3)
+
 def callback(msg):
-    cloud = np.fromstring(msg.data, dtype=np.float32)
-    cloud = cloud.reshape(cloud.size/3, 3)
-    test.merge(cloud, transformation( "/map", msg.header ))
+    start = time.time()
+    test.merge(cloud(msg.data), transformation("/map", msg.header))
+    profile.append(time.time() - start)
 
 rospy.init_node("atlaas_ros")
 tfl = TransformListener()
 rospy.Subscriber("/robot/camera", PointCloud2, callback)
-rospy.spin() # this will block untill you hit Ctrl+C
+try:
+    rospy.spin() # this will block untill you hit Ctrl+C
+finally:
+    test.save_currents()
+    print("merge min/max/avg = %.3f / %.3f / %.3f ms" % \
+        ( 1000*min(profile), 1000*max(profile),
+          1000*sum(profile) / len(profile) ) )
