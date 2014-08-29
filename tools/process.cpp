3eb2568dd8488cae9e646cf0d4b4bc504a76f902
Pierrick Koch <pierrick.koch@gmail.com>
2014-08-29 14:11:39 +0200

    [tools] add process.cpp

---
+++ tools/process.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <atlaas/atlaas.hpp>
+
+int main(int argc, char * argv[]) {
+    atlaas::atlaas obj;
+    obj.init(120.0, 120.0, 0.1, 0, 0, 0, 31, true);
+    obj.process_pcd();
+    obj.save_currents();
+    return 0;
+}
