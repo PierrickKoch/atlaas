283123410b93356beac56880af6a2715ec9a426d
Pierrick Koch <pierrick.koch@gmail.com>
2014-09-10 17:17:48 +0200

    [tools] add pcd <-> atlaas convert utils

---
+++ tools/atlaas2pcd.cpp
@@ -0,0 +1,21 @@
+#include <cstdlib>
+#include <iostream>
+#include <atlaas/io.hpp>
+#include <atlaas/pcd.hpp>
+
+int main(int argc, char * argv[]) {
+    if (argc < 3) {
+        std::cerr << "usage: " << argv[0] << " in.apc out.pcd" << std::endl;
+        return EXIT_FAILURE;
+    }
+    atlaas::points cloud;
+    atlaas::matrix transformation;
+    try {
+        atlaas::load(argv[1], cloud, transformation);
+        atlaas::write_pcd(argv[2], cloud, transformation);
+    } catch (std::exception& e) {
+        std::cerr << "[error] " << e.what();
+        return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
+}
---
+++ tools/pcd2atlaas.cpp
@@ -0,0 +1,21 @@
+#include <cstdlib>
+#include <iostream>
+#include <atlaas/io.hpp>
+#include <atlaas/pcd.hpp>
+
+int main(int argc, char * argv[]) {
+    if (argc < 3) {
+        std::cerr << "usage: " << argv[0] << " in.pcd out.apc" << std::endl;
+        return EXIT_FAILURE;
+    }
+    atlaas::points cloud;
+    atlaas::matrix transformation;
+    try {
+        atlaas::read_pcd(argv[1], cloud, transformation);
+        atlaas::save(argv[2], cloud, transformation);
+    } catch (std::exception& e) {
+        std::cerr << "[error] " << e.what();
+        return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
+}
