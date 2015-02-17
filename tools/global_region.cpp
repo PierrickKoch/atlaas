52c1f2693c3aecdba1622702014b7aa6717c6949
Pierrick Koch <pierrick.koch@gmail.com>
2015-02-17 18:19:39 +0100

    [tools] add decimate function in global region
    takes about 0.37 sec avg. for the same dataset (41 tiles, 280*280m)

--- tools/global_region.cpp
+++ tools/global_region.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
-#include <atlaas/atlaas.hpp>
+#include <atlaas/common.hpp>
 #include <gdalwrap/gdal.hpp>
 
 static const float VARIANCE_THRESHOLD = 0.01;
-static const float EDGE_FACTOR = 1337;
+static const float EDGE_FACTOR = 100;
 static const float NO_DATA = -10000;
 
 void select(std::vector<gdalwrap::gdal>& files, float vt = VARIANCE_THRESHOLD) {
@@ -20,33 +20,56 @@ void select(std::vector<gdalwrap::gdal>& files, float vt = VARIANCE_THRESHOLD) {
     }
 }
 
-void scale(std::vector<gdalwrap::gdal>& files) {
-    // TODO scale down 10% (to a metric scale)q
-}
-
-std::vector<uint8_t> edge(const gdalwrap::raster& r, size_t sx, size_t sy,
-        float factor = EDGE_FACTOR) {
-    size_t cx = 0;
-    float r1, r2, r3;
-    std::vector<uint8_t> bytes(r.size());
-    auto bit = bytes.begin();
-    for (auto rit = r.begin(), end = r.end() - sx - 1; rit < end; rit++, bit++) {
-        if (cx == sx - 1) {
-            cx = 0;
-        } else if (*rit > NO_DATA) {
-            cx++;
-            r1 = *rit - *(rit + 1);
-            r2 = *rit - *(rit + sx);
-            r2 = *rit - *(rit + sx + 1);
-            *bit = std::min(255.0f, 1 + (r1*r1 + r2*r2 + r3*r3) * factor);
+void decimate(gdalwrap::gdal& region, float scale = 5) {
+    size_t sx = region.get_width(), sy = region.get_height();
+    auto it = region.bands[0].begin(), new_it = it;
+    for (size_t i = 0; i < sy; i+=scale) {
+        for (size_t j = 0; j < sx; j+=scale, it++) {
+            for (int u = 0; u < scale; u++) {
+                for (int v = 0; v < scale; v++) {
+                    *it = std::max(*it, *(new_it + j + v + (i + u) * sy));
+                }
+            }
         }
     }
-    return bytes;
+    region.set_transform(region.get_utm_pose_x(), region.get_utm_pose_y(),
+        region.get_scale_x() * scale, region.get_scale_y() * scale);
+    region.set_size(1, sx / scale, sy / scale);
+}
+
+void edge(gdalwrap::gdal& region, float factor = EDGE_FACTOR) {
+    size_t sx = region.get_width();
+    for (auto rit = region.bands[0].begin(), rit1 = rit + 1, rit2 = rit + sx,
+            rit3 = rit2 + 1, end = region.bands[0].end(); rit3 < end; rit++,
+            rit1++, rit2++, rit3++) {
+        if (*rit > NO_DATA) {
+            *rit = std::min(255.0f, 1 + factor *
+                (std::abs(*rit - *rit1) +
+                 std::abs(*rit - *rit2) +
+                 std::abs(*rit - *rit3) ));
+        } else {
+            *rit = 0;
+        }
+    }
+}
+
+void region(std::vector<gdalwrap::gdal>& files, const std::string filepath) {
+    // select only dtm band
+    select(files);
+    // merge all files
+    gdalwrap::gdal result = gdalwrap::merge(files, NO_DATA);
+    // edge detect
+    edge(result);
+    // scale down
+    decimate(result);
+    // convert to grayscale PNG
+    std::vector<uint8_t> gray(result.bands[0].begin(), result.bands[0].end());
+    result.export8u(filepath, gray, "PNG");
 }
 
 int main(int argc, char * argv[]) {
     if (argc < 4) {
-        std::cerr << "usage: " << argv[0] << " file1.tif file2.tif ... out.tif"
+        std::cerr << "usage: " << argv[0] << " file1.tif file2.tif ... out.png"
                   << std::endl;
         return 1;
     }
@@ -56,14 +79,6 @@ int main(int argc, char * argv[]) {
     for (int filen = 1; filen < argc - 1; filen++) {
         files[filen - 1].load( argv[filen] );
     }
-    // select only dtm band
-    select(files);
-    // scale down 10% (to a metric scale)
-    // TODDO scale(files);
-    // merge all files
-    gdalwrap::gdal result = merge(files);
-    // edge detect / sobel like
-    result.export8u(argv[argc - 1],
-        edge(result.bands[0], result.get_width(), result.get_height()), "PNG");
+    region(files, argv[argc - 1]);
     return 0;
 }
