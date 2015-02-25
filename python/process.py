95ce85d458264542a6569d36715a4c083ee4719f
Pierrick Koch <pierrick.koch@gmail.com>
2015-02-25 10:21:34 +0100

    [python] process now save region.png
    stats for 2014_08_20_16_41_18_velodyne_pcd2 dataset:
    533 clouds, ADREAM parking, Velodyne HDL 64, ~110K points/cloud
    
    process region    277.018 ms
    process cloud avg 17.842 ms

--- python/process.py
+++ python/process.py
@@ -1,4 +1,8 @@
 #! /usr/bin/env python
+"""
+Will process all cloud.%5i.{raw,pcd} in ${ATLAAS_PATH,.}
+Then save tiles and region.png
+"""
 import time
 import atlaas
 
@@ -9,9 +13,10 @@ start = time.time()
 npcds = test.process()
 total = time.time() - start
 
-test.save_currents()
-
-print( "process avg %.3f ms" % (1000. * total / npcds) )
+start = time.time()
+test.region('region.png')
+print( "process region    %.3f ms" % (1000. * (time.time() - start)))
+print( "process cloud avg %.3f ms" % (1000. * total / npcds) )
 
 # gdal_merge.py -co "COMPRESS=DEFLATE" atlaas.*x*.tif
 # gdaldem hillshade -b 4 out.tif -of PNG out.hillshade.png
