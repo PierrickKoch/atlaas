8d32bfcd93df22a06db1d0f49fa104156ba7e16d
Pierrick Koch <pierrick.koch@gmail.com>
2015-04-06 16:09:46 +0200

    [python] test reprocess w/cov

--- python/test/test_reprocess.py
+++ python/test/test_reprocess.py
@@ -6,12 +6,13 @@ test = atlaas.Atlaas()
 test.init(120.0, 120.0, 0.1, 0, 0, 0, 31, True)
 transformation = np.identity(4, dtype=np.double)
 cloud = np.random.rand(2000, 3).astype('float32')
+covariance = np.array([0]*36, dtype=np.float64).reshape(6, 6)
 
 time_ms = []
 for i in range(100):
     time_ms.append( int(time.time()*1000) )
     transformation[0:2,3] = i
-    test.merge(cloud, transformation)
+    test.merge(cloud, transformation, covariance)
 
 start = time.time()
 npcds = test.reprocess(time_ms[10], time_ms[30], 20, 80)
