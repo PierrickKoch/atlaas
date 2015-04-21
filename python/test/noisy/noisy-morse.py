907ef77dab577c6e612237b788c52c0474d3f9e9
Pierrick Koch <pierrick.koch@gmail.com>
2015-04-21 12:04:47 +0200

    [python] test noisy add gyro drift

--- python/test/noisy/noisy-morse.py
+++ python/test/noisy/noisy-morse.py
@@ -12,8 +12,8 @@ robot.append(odom)
 odom.add_stream('ros', method='morse.middleware.ros.pose.TFPublisher')
 odom.add_stream('ros', method='morse.middleware.ros.pose.PoseWithCovarianceStampedPublisher')
 odom.alter(classpath='morse.modifiers.odometry_noise.OdometryNoiseModifier',
-    factor = 1, factor_sigma = 0.5)
-#    gyro_drift = 0.001, gyro_drift_sigma = 0.0001)
+    factor = 1, factor_sigma = 0.5,
+    gyro_drift = 0.00001, gyro_drift_sigma = 0.0001)
 odom.add_service('socket')
 
 camera = Velodyne()
