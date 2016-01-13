138885aeb0c98caa13ae943b98dcb892fd7b8c01
Pierrick Koch <pierrick.koch@gmail.com>
2016-01-13 15:07:35 +0100

    [python] add tile to region job
    meant to be a background process, like::
    
        while true; do python tile_to_region.py; sleep 1; done

---
+++ python/tile_to_region.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python
+"""
+tile_to_region
+==============
+
+*transform dtm tiles to region-map tiles*
+
+usage: while true; do python tile_to_region.py; sleep 1; done
+"""
+import os
+import glob
+import atlaas
+
+need_merge = False
+atlaas_tiles = glob.glob("atlaas.*x*.tif")
+for fatlaas in atlaas_tiles:
+    fregion = "region.%s.png"%fatlaas.split('.')[1]
+    print(fregion)
+    need_convert = True
+    if os.path.isfile(fregion):
+        satlaas = os.stat(fatlaas)
+        sregion = os.stat(fregion)
+        if satlaas.st_mtime <= sregion.st_mtime:
+            need_convert = False
+    if need_convert:
+        atlaas.tile_to_region(fatlaas, fregion)
+        need_merge = True
+
+if need_merge:
+    atlaas.merge("region.*x*.png", "region.png")
