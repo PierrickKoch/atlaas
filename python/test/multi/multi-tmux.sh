68be51e65be754b71e36e7096a0a4848d26ee29c
Pierrick Koch <pierrick.koch@gmail.com>
2016-01-13 15:38:25 +0100

    [python] test multi add pypero demo

--- python/test/multi/multi-tmux.sh
+++ python/test/multi/multi-tmux.sh
@@ -4,6 +4,7 @@
 [ -e momo ] && mv momo momo.`date +%s`
 mkdir mana
 mkdir momo
+export MORSE_SILENT_PYTHON_CHECK=1
 tmux att -t multi || tmux \
     new-session -s multi \; \
     send-keys "source /opt/ros/indigo/setup.bash" C-m \; \
@@ -15,8 +16,24 @@ tmux att -t multi || tmux \
     send-keys "source /opt/ros/indigo/setup.bash" C-m \; \
     send-keys "rosrun rviz rviz -d multi-rviz.rviz" C-m \; \
     new-window \; \
+    send-keys "cd mana; python -m SimpleHTTPServer 8042" C-m \; \
+    split-window \; \
+    send-keys "cd momo; python -m SimpleHTTPServer 8043" C-m \; \
+    new-window \; \
+    send-keys "cd mana; while true; do python ~/work/atlaas/python/tile_to_region.py; sleep 1; done" C-m \; \
+    split-window \; \
+    send-keys "cd momo; while true; do python ~/work/atlaas/python/tile_to_region.py; sleep 1; done" C-m \; \
+    new-window \; \
     send-keys "source /opt/ros/indigo/setup.bash" C-m \; \
     send-keys "python multi-ros.py mana" C-m \; \
     split-window \; \
     send-keys "source /opt/ros/indigo/setup.bash" C-m \; \
     send-keys "python multi-ros.py momo" C-m \; \
+    new-window \; \
+    send-keys "cd mana" C-m \; \
+    send-keys "export PYPERO_LIST=http://localhost:8043" C-m \; \
+    send-keys "python ~/work/atlaas/tools/pypero.py 123.4 567.8 -98.7 -65.4" C-m \; \
+    split-window \; \
+    send-keys "cd momo" C-m \; \
+    send-keys "export PYPERO_LIST=http://localhost:8042" C-m \; \
+    send-keys "python ~/work/atlaas/tools/pypero.py 123.4 567.8 -98.7 -65.4" C-m \; \
