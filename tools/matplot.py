6c13c30ccfc91b7cb1de209175e93aff9e30237b
Pierrick Koch <pierrick.koch@gmail.com>
2016-02-29 16:12:50 +0100

    [tools] update ipython/jupyter/matplotlib helpers

--- tools/matplot.py
+++ tools/matplot.py
@@ -1,31 +1,59 @@
-# -*- coding: utf-8 -*-
-# <nbformat>3.0</nbformat>
+# coding: utf-8
 
-# <codecell>
-
+get_ipython().magic(u'matplotlib inline')
 filename = 'out.tif'
 
 import gdal
-# run `ipython notebook --pylab inline`
+import numpy as np
 import matplotlib.pyplot as plt
-names = {v:k for k,v in enumerate(["N_POINTS", "Z_MIN", "Z_MAX", "Z_MEAN", "VARIANCE", "TIME", "DIST_SQ"])}
-geotiff = gdal.Open( filename )
-bands   = geotiff.ReadAsArray()
 
-def show_hist(name, fsize=(12, 5)):
-    band = bands[names[name]]
-    plt.figure(figsize = fsize)
-    plt.hist( band.flatten(), 100, log=1 )
-    plt.show()
+class gdal2:
+    def __init__(self, filepath):
+        self.filepath = filepath
+        self.geodata = gdal.Open( filepath )
+        self.bands = self.geodata.ReadAsArray()
+        bn = [self.geodata.GetRasterBand(i).GetMetadata().get('NAME', str(i)) \
+              for i in range(1, self.geodata.RasterCount+1)]
+        self.names = {v:k for k,v in enumerate(bn)}
+        self.transform = geodata.GetGeoTransform()
+        self.meta      = geodata.GetMetadata()
+        self.scale_x = self.transform[1]
+        self.scale_y = self.transform[5]
+        self.utm_x   = self.transform[0]
+        self.utm_y   = self.transform[3]
+        self.custom_x_origin = float(self.meta['CUSTOM_X_ORIGIN'])
+        self.custom_y_origin = float(self.meta['CUSTOM_Y_ORIGIN'])
 
-def show_band(name, cmin=0, cmax=1, cmap='spectral', fsize=(14, 14)):
-    band = bands[names[name]]
+    def u2p(self, x, y):
+        return ((x - self.utm_x) / self.scale_x,
+                (y - self.utm_y) / self.scale_y)
+    def p2u(self, x, y):
+        return (x * self.scale_x + self.utm_x,
+                y * self.scale_y + self.utm_y)
+    def c2p(self, x, y):
+        return u2p(*self.c2u(x, y))
+    def p2c(gdal, x, y):
+        return u2c(*self.p2u(x, y))
+    def c2u(self, x, y):
+        return (x + self.custom_x_origin,
+                y + self.custom_y_origin)
+    def u2c(self, x, y):
+        return (x - self.custom_x_origin,
+                y - self.custom_y_origin)
+
+def hist(data, fsize=(12, 5)):
     plt.figure(figsize = fsize)
-    imgplot = plt.imshow( band )
+    plt.hist( data.flatten(), 100, log=1 )
+
+def show(data, cmin=0, cmax=1, cmap='viridis', fsize=(14, 12), x=[], y=[]):
+    plt.figure(figsize = fsize)
+    imgplot = plt.imshow( data, interpolation='none' )
     imgplot.set_clim(cmin, cmax)
     imgplot.set_cmap(cmap)
     plt.colorbar()
-    plt.show()
+    if len(x) and len(y):
+        plt.plot(x, y, 'r', scalex=False, scaley=False)
+    plt.tight_layout(pad=0)
 
 # <codecell>
 
