3927e1aa445298ed8142aaade523720acfa2086d
Pierrick Koch <pierrick.koch@gmail.com>
2016-03-01 14:59:14 +0100

    [python] add gladys2 in atlaas.helpers

---
+++ python/atlaas/helpers/gladys.py
@@ -0,0 +1,16 @@
+import gladys
+
+class gladys2:
+    def __init__(self, region, robot):
+        self.ctmap = gladys.costmap(region, robot)
+        self.graph = gladys.nav_graph(self.ctmap)
+        self.gdmap = self.ctmap.get_map()
+    def c2u(self, x, y):
+        return gladys.point_custom2utm(self.gdmap, x, y)
+    def u2c(self, x, y):
+        return gladys.point_utm2custom(self.gdmap, x, y)
+    def path(self, a, b):
+        # A* a, b
+        res_utm, cost = self.graph.search_with_cost(self.c2u(*a), self.c2u(*b))
+        res = [self.u2c(x, y) for x, y in res_utm]
+        return cost, res
