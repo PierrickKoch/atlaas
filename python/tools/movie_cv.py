e4a438f3aa636b4654d18aacd22b54ba9c92153d
Pierrick Koch <pierrick.koch@gmail.com>
2016-03-02 12:47:12 +0100

    [python] tools add movie w/ opencv

---
+++ python/tools/movie_cv.py
@@ -0,0 +1,52 @@
+#! /usr/bin/env python
+import gdal
+import Image
+import atlaas
+import subprocess
+import numpy as np
+from matplotlib import cm, pyplot
+import os
+import cv2
+
+class TrackbarData:
+    def __init__(self, val=0):
+        self.value = val
+    def create(self, name='Trackbar', window='Window'):
+        cv2.createTrackbar(name, window, int(self.value), 10, self.update)
+    def update(self, val):
+        self.value = val
+
+T1 = TrackbarData(0)
+T2 = TrackbarData(10)
+cv2.namedWindow('image')
+T1.create('T1', 'image')
+T2.create('T2', 'image')
+
+def convert(fin, cmin, cmax, cmap=cm.spectral):
+    geo = gdal.Open(fin)
+    img = geo.ReadAsArray() # get band as a numpy.array
+    img = (img - cmin) * (1./(cmax - cmin))
+    img[img > 1] = 1
+    img[img < 0] = 0
+    return np.uint8(cmap(img)*255)
+
+pcd_id = 0
+patern = 'cloud.%05i.pcd'
+ftif = 'atlaas.zmean.tif'
+
+test = atlaas.Atlaas()
+test.init(120.0, 120.0, 0.1, 0, 0, 0, 31, True)
+
+while os.path.isfile(patern%pcd_id):
+    test.merge_file(patern%pcd_id)
+    test.export_zmean(ftif)
+    image = convert(ftif, T1.value, T2.value)
+    if not pcd_id:
+        fourcc = cv2.cv.CV_FOURCC(*'FMP4')
+        size = (image.shape[1], image.shape[0]) # tuple(reversed(image.shape))
+        video = cv2.VideoWriter('atlaas.zmean.avi', fourcc, 100, size)
+    video.write( image )
+    cv2.imshow('image', image)
+    pcd_id += 1
+    if cv2.waitKey(1) & 0xFF == 27: # aka ESCAPE
+        break
