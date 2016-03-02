4b58a00bb254dc48de4929555fc891ddc9e04a88
Pierrick Koch <pierrick.koch@gmail.com>
2016-03-02 16:28:55 +0100

    [python] add atlaas OccupancyGrid publisher
    from repo work with Ellon

---
+++ python/atlaas/helpers/rosgrid.py
@@ -0,0 +1,43 @@
+import numpy
+from atlaas.helpers.gdal2 import gdal2
+from nav_msgs.msg import OccupancyGrid
+
+def atlaas8u_grid(filepath, seq=None, stamp=None, frame_id='/map'):
+    g = gdal2(filepath)
+    b = g.bands[0] if len(g.bands.shape) > 2 else g.bands
+    og = OccupancyGrid()
+    og.data = (b.astype('float')/2.55).astype('uint8').flatten()
+    if seq: og.header.seq = seq
+    if stamp: og.header.stamp = stamp
+    og.header.frame_id = frame_id
+    og.info.resolution = g.scale_x
+    og.info.height, og.info.width = b.shape
+    og.info.origin.position.x, og.info.origin.position.y = g.u2c(g.utm_x, g.utm_y)
+    og.info.origin.orientation.x = 1 # flip to transform UTM-ROS (scale_y < 0)
+    return og
+
+def atlaas_grid(filepath, var_threshold=0.1, seq=None, stamp=None, frame_id='/map'):
+    """ Usage:
+    atlaas.merge("atlaas.*x*.tif", "out.tif")
+    if os.path.isfile("out.tif"):
+        atlaas_grid("out.tif")
+    """
+    g = gdal2(filepath)
+    bnp = g.bands[g.names['N_POINTS']]
+    bma = g.bands[g.names['Z_MAX']]
+    bme = g.bands[g.names['Z_MEAN']]
+    bva = g.bands[g.names['VARIANCE']]
+    data = numpy.where(bva > var_threshold, bma, bme)
+    delta = data.max() - data.min()
+    ddis = (100.0*(data-data.min())/delta).astype('uint8')
+    ddis[bnp < 1] = 0 # no points, unknown
+    og = OccupancyGrid()
+    og.data = ddis.flatten()
+    if seq: og.header.seq = seq
+    if stamp: og.header.stamp = stamp
+    og.header.frame_id = frame_id
+    og.info.resolution = g.scale_x
+    og.info.height, og.info.width = data.shape
+    og.info.origin.position.x, og.info.origin.position.y = g.u2c(g.utm_x, g.utm_y)
+    og.info.origin.orientation.x = 1 # flip to transform UTM-ROS (scale_y < 0)
+    return og
---
+++ python/tools/atlaas_rosgrid.py
@@ -0,0 +1,42 @@
+#! /usr/bin/env python
+"""
+Atlaas to OccupancyGrid node
+The idea is to publish 2 grids:
+- one resulting of dumped tiles (GeoTiff)
+- one from the current tiles in memory (via export8u)
+
+NOTE those are not OccupancyGrid, just distribution of elevation (debug only)
+"""
+import os
+import atlaas
+from atlaas.helpers.rosgrid import atlaas8u_grid, atlaas_grid
+import rospy
+from nav_msgs.msg import OccupancyGrid
+
+mtime = 0
+jpg_pose = None
+rospy.init_node("atlaas_grid")
+atlaas_path = os.environ.get("ATLAAS_PATH", ".")
+filepath = os.path.join(atlaas_path, "atlaas.jpg")
+mergedpath = os.path.join(atlaas_path, "out.tif")
+pub = rospy.Publisher("/grid", OccupancyGrid, queue_size=5, latch=True)
+pub_glob = rospy.Publisher("/grid_glob", OccupancyGrid, queue_size=5, latch=True)
+
+while not rospy.is_shutdown():
+    rospy.sleep(0.1)
+    if not pub.get_num_connections() and not pub_glob.get_num_connections():
+        continue # skip if no client
+    filestat = os.stat(filepath)
+    if filestat.st_mtime == mtime:
+        continue # skip if not modified
+    mtime = filestat.st_mtime
+    grid = atlaas8u_grid(filepath, stamp=rospy.Time.now())
+    pose = grid.info.origin.position.x, grid.info.origin.position.y
+    pub.publish(grid)
+    if not jpg_pose or pose != jpg_pose:
+        jpg_pose = pose
+        # if we did atlaas::slide (the internal tiles in memory moved)
+        atlaas.merge(os.path.join(atlaas_path, "atlaas.*x*.tif"), mergedpath)
+        if not os.path.isfile(mergedpath):
+            continue # skip if no tiles on disk
+        pub_glob.publish(atlaas_grid(mergedpath, stamp=rospy.Time.now()))
