8d9a185f4b873903e526c1461f485a553a7fa02a
Pierrick Koch <pierrick.koch@gmail.com>
2016-04-21 16:28:58 +0200

    [python] add title option in matplot show helper

--- python/atlaas/helpers/matplot.py
+++ python/atlaas/helpers/matplot.py
@@ -5,7 +5,7 @@ def hist(data, fsize=(12, 5)):
     plt.hist( data.flatten(), 100, log=1 )
 
 def show(data, cmin=0, cmax=1, cmap='viridis', fsize=(14, 12), x=[], y=[],
-         pad=None, fname=None):
+         pad=None, fname=None, title=None):
     plt.figure(figsize = fsize)
     imgplot = plt.imshow( data, interpolation='none' )
     imgplot.set_clim(cmin, cmax)
@@ -15,5 +15,7 @@ def show(data, cmin=0, cmax=1, cmap='viridis', fsize=(14, 12), x=[], y=[],
         plt.plot(x, y, 'r', scalex=False, scaley=False)
     if pad:
         plt.tight_layout(pad=pad)
+    if title:
+        plt.title(title)
     if fname:
         plt.savefig(fname)
