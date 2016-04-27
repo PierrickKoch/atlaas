12146a9cad6624bff16e61e0d38f053cdce66bf6
Pierrick Koch <pierrick.koch@gmail.com>
2016-04-27 09:40:49 +0200

    [python] helpers image support LA mode
    patch sent at:
    https://github.com/python-pillow/Pillow/pull/1865

--- python/atlaas/helpers/image.py
+++ python/atlaas/helpers/image.py
@@ -5,10 +5,20 @@ except ImportError:
     import Image # aka Pillow
 
 def save(path, data, quality=90):
+    if len(data.shape) == 3 and data.shape[2] == 2:
+        # Pillow "Cannot handle this data type" mode=LA
+        img = Image.fromarray(data[:,:,0])
+        alp = Image.fromarray(data[:,:,1])
+        img.putalpha(alp)
+    else:
+        img = Image.fromarray(data)
     # in case of JPEG or WebP, set quality to 90%, else this option is ignored
-    Image.fromarray(data).save(path, quality=quality)
+    img.save(path, quality=quality)
 
 def load(path, dtype='uint8'):
     img = Image.open(path)
     arr = numpy.array(img.getdata(), dtype)
     return arr.reshape(img.size[1], img.size[0], arr.shape[1])
+
+def zero(path):
+    save(path, load(path) * 0)
