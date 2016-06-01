59296eb643f9f71e35ceb351f6118f6adcdc6dd9
Pierrick Koch <pierrick.koch@gmail.com>
2016-06-01 10:50:08 +0200

    [python] helpers matplot add style option

--- python/atlaas/helpers/matplot.py
+++ python/atlaas/helpers/matplot.py
@@ -5,14 +5,14 @@ def hist(data, fsize=(12, 5)):
     plt.hist( data.flatten(), 100, log=1 )
 
 def show(data, cmin=0, cmax=1, cmap='viridis', fsize=(14, 12), x=[], y=[],
-         pad=None, fname=None, title=None):
+         style='r', pad=None, fname=None, title=None):
     plt.figure(figsize = fsize)
     imgplot = plt.imshow( data, interpolation='none' )
     imgplot.set_clim(cmin, cmax)
     imgplot.set_cmap(cmap)
     plt.colorbar()
     if len(x) and len(y):
-        plt.plot(x, y, 'r', scalex=False, scaley=False)
+        plt.plot(x, y, style, scalex=False, scaley=False)
     if pad:
         plt.tight_layout(pad=pad)
     if title:
