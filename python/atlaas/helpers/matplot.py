026e62f979b2b43b0967bcd8c01708917c18dbb2
Pierrick Koch <pierrick.koch@gmail.com>
2016-07-11 16:30:06 +0200

    [python] helpers matplot.hist filter NaNs

--- python/atlaas/helpers/matplot.py
+++ python/atlaas/helpers/matplot.py
@@ -1,8 +1,11 @@
+import numpy
 import matplotlib.pyplot as plt
 
 def hist(data, fsize=(12, 5)):
     plt.figure(figsize = fsize)
-    plt.hist( data.flatten(), 100, log=1 )
+    D = data.flatten()
+    D = D[~numpy.isnan(D)]
+    plt.hist(D, 100, log=1)
 
 def show(data, cmin=0, cmax=1, cmap='viridis', fsize=(14, 12), x=[], y=[],
          style='r', pad=None, fname=None, title=None):
